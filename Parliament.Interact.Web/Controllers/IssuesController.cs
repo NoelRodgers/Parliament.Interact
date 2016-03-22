bd69e409af105fe295e592f843fa413e3fa9ddb5
APPLEYARD <appleyardr@parliament.uk>
2016-03-22 16:28:38 +0000

    Added IssuesController

--- Parliament.Interact.Web/Controllers/IssuesController.cs
+++ Parliament.Interact.Web/Controllers/IssuesController.cs
@@ -1,7 +1,20 @@
-﻿namespace Parliament.Interact.Web.Controllers
+﻿using System.Web.Mvc;
+using Parliament.Interact.Core.Services;
+
+namespace Parliament.Interact.Web.Controllers
 {
-    public class IssuesController
+    public class IssuesController : Controller
     {
-         
+        private readonly IIssueService _issueService;
+
+        public IssuesController(IIssueService issueService)
+        {
+            _issueService = issueService;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
     }
 }
