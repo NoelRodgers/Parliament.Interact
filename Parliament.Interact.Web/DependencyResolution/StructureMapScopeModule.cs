bea51f6ed030d7654f8582386b57125b94987a28
NoelRodgers <njrwork@hotmail.co.uk>
2016-03-22 14:11:35 +0000

    Adding the three projects to get started on Interact

---
+++ Parliament.Interact.Web/DependencyResolution/StructureMapScopeModule.cs
@@ -0,0 +1,24 @@
+namespace Parliament.Interact.Web.DependencyResolution {
+    using System.Web;
+
+    using Parliament.Interact.Web.App_Start;
+
+    using StructureMap.Web.Pipeline;
+
+    public class StructureMapScopeModule : IHttpModule {
+        #region Public Methods and Operators
+
+        public void Dispose() {
+        }
+
+        public void Init(HttpApplication context) {
+            context.BeginRequest += (sender, e) => StructuremapMvc.StructureMapDependencyScope.CreateNestedContainer();
+            context.EndRequest += (sender, e) => {
+                HttpContextLifecycle.DisposeAndClearAll();
+                StructuremapMvc.StructureMapDependencyScope.DisposeNestedContainer();
+            };
+        }
+
+        #endregion
+    }
+}
