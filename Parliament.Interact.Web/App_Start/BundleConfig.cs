522e27fe90046ea3f950e95509d7113d327fda65
DESANTIS <desantisg@parliament.uk>
2016-03-23 12:08:38 +0000

    started styling index page columns

--- Parliament.Interact.Web/App_Start/BundleConfig.cs
+++ Parliament.Interact.Web/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace Parliament.Interact.Web
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/interact.css"));
         }
     }
 }
