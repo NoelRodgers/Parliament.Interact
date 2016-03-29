a8561b62547280d12350117524fa05aa3591af59
APPLEYARD <appleyardr@parliament.uk>
2016-03-29 12:06:53 +0100

    Intro content now coming through from database for each issue.  Displays HTML tags correctly

--- Parliament.Interact.Core/Migrations/Configuration.cs
+++ Parliament.Interact.Core/Migrations/Configuration.cs
@@ -44,13 +44,13 @@ namespace Parliament.Interact.Core.Migrations
                 },
                 new Issue
                 {
-                    Title = "Academy Schools",
-                    Content = "Description of Test Issue 4"
+                    Title = "Housing & Planning",
+                    Content = "Description of Test Issue 5"
                 },
                 new Issue
                 {
-                    Title = "Housing & Planning",
-                    Content = "Description of Test Issue 5"
+                    Title = "Academy Schools",
+                    Content = "<p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.</p>"
                 }
             };
 
