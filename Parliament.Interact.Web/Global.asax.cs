0015f9e8904a708a925946841bc1bd2f6edbc80e
NoelRodgers <njrwork@hotmail.co.uk>
2016-03-29 17:18:54 +0100

    Adding error logging

--- Parliament.Interact.Web/Global.asax.cs
+++ Parliament.Interact.Web/Global.asax.cs
@@ -5,17 +5,25 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using NLog;
 
 namespace Parliament.Interact.Web
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        private readonly ILogger _logger = LogManager.GetCurrentClassLogger();
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_Error()
+        {
+            _logger.Error(Server.GetLastError());
         }
     }
 }
